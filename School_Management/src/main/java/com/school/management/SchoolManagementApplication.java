4d7212d63eed977d299335fe81f7cde0b4097e77
Ramcharan2012 <153090162+Ramcharan2012@users.noreply.github.com>
2026-03-01 18:52:49 +0530

    Updated Author

--- School_Management/src/main/java/com/school/management/SchoolManagementApplication.java
+++ School_Management/src/main/java/com/school/management/SchoolManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//@Authour : Ramcharan
+//@Authour : Ramcharan Teja
 @SpringBootApplication
 public class SchoolManagementApplication {
 
