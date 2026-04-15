4891f5d5d834a2a1cbbfa54afa97bd9b75710a1c
RAMCHARAN <ramcharantejapandavula@gmail.com>
2026-04-15 22:46:13 +0530

    fix: limit Kafka/Zookeeper memory, make Spring Boot Kafka non-blocking

---
+++ TestHash.java
@@ -0,0 +1,7 @@
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+public class TestHash {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("Admin@123"));
+    }
+}
